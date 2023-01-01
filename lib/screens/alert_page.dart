1ca2e4857b9fbeb998c45ccf9195b2177400bd23
illika <steve.milan7@gmail.com>
2023-01-01 17:35:04 -0500

    alert y uso de Platform

--- lib/screens/alert_page.dart
+++ lib/screens/alert_page.dart
@@ -1,7 +1,83 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
   const AlertPage({Key? key}) : super(key: key);
+
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (_) {
+        return CupertinoAlertDialog(
+          title: const Text("Title"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text("Content"),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                "Cerrar",
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Ok"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void displayDialogAndroid(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: const Text("Title"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text("Content"),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusDirectional.circular(15),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                "Cerrar",
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Ok"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +85,9 @@ class AlertPage extends StatelessWidget {
       child: Scaffold(
         body: Center(
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () => Platform.isAndroid
+                ? displayDialogAndroid(context)
+                : displayDialogIOS(context),
             child: const Text("Mostrar Alerta"),
           ),
         ),
