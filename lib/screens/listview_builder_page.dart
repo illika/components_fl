1ea14eaaff5190a23196d730fd31d0fb91e3cde6
illika <steve.milan7@gmail.com>
2023-01-03 18:47:34 -0500

    ListViewBuilder - InfiniteScroll

--- lib/screens/listview_builder_page.dart
+++ lib/screens/listview_builder_page.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListviewBuilderPage extends StatelessWidget {
+class ListviewBuilderPage extends StatefulWidget {
   const ListviewBuilderPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListviewBuilderPage> createState() => _ListviewBuilderPageState();
+}
+
+class _ListviewBuilderPageState extends State<ListviewBuilderPage> {
+  final List<int> imagesId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      debugPrint(
+          "${_scrollController.position.pixels} - ${_scrollController.position.maxScrollExtent}");
+      if (_scrollController.position.pixels + 500 >=
+          _scrollController.position.maxScrollExtent) {
+        add10();
+      }
+    });
+  }
+
+  void add10() {
+    final lastId = imagesId.last;
+    imagesId.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +39,18 @@ class ListviewBuilderPage extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          physics: const BouncingScrollPhysics(),
+          controller: _scrollController,
+          itemCount: imagesId.length,
           itemBuilder: (_, int index) {
             return FadeInImage(
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-                placeholder: const AssetImage("jar-loading.gif"),
-                image: NetworkImage(
-                    "https://picsum.photos/500/300?random=${index + 1}"));
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+              placeholder: const AssetImage("jar-loading.gif"),
+              image: NetworkImage(
+                  "https://picsum.photos/500/300?random=${imagesId[index]}"),
+            );
           },
         ),
       ),
